004fa0f1dc7c3944a47994bb98c6c74693e537e6
Danny Summerlin <hello@craftedbycreo.com>
2014-02-10 11:00:39 -0500

    made POST and GET invoiceId the same

--- invoice.php
+++ invoice.php
@@ -1,95 +1,136 @@
 <?php
-    require('stripe/Stripe.php');
+//common settings
+$companyName = "Creo LLC";
+$address = <<<EOD
+Street 1<br>
+Street 2<br>
+City, State Zip<br>
+EOD;
+$phone = "407-603-6432";
+$baseURL = "http://craftedbycreo.com";
+$baseEmail = "hello@craftedbycreo.com";
+$icon = $baseURL."/assets/img/favicon.png";
+$stripeKey = "sk_test_";
+$publicKey = "pk_test_";
 
-    $invoiceID = 'in_123456789';
-    $stripeKey = "{PRIVATE_KEY}";
+	$invoiceID = $_POST["i"];
+	if(!isset($invoiceID))
+		$invoiceID = $_GET['i'];
+	if($invoiceID !== "in_") {
+		$outputInvoice = true;
+		require('stripe/Stripe.php');
+		Stripe::setApiKey($stripeKey);
 
-
-    date_default_timezone_set('UTC');
-    Stripe::setApiKey($stripeKey);
-  
-    //$this->printArray($in);
-    $invoice = Stripe_Invoice::retrieve($invoiceID);
-
+		$invoice = Stripe_Invoice::retrieve($invoiceID);
+		$customer = Stripe_Customer::retrieve($invoice->customer);
+		$alreadyPaid = $invoice->closed;
+		$token = $_POST['stripeToken'];
+		if(isset($token)) {
+			try {
+				$charge = Stripe_Charge::create(array(
+					"amount" => $invoice->total,
+					"currency" => "usd",
+					"card" => $token,
+					"description" => $customer->email
+				));
+				$invoice->closed = true;
+				$invoice->save();
+				$showThanks = true;
+			} catch(Stripe_CardError $e) {
+			// The card has been declined
+				$cardError = true;
+			}
+		}
+	}
+	if($outputInvoice) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Invoice</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Customer Invoice">
-    <meta name="author" content="5marks">
+	<head>
+	<meta charset="utf-8">
+	<title>Customer Invoice</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Customer Invoice">
+	<meta name="author" content="<?= $companyName ?>">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+	<style>
+		.invoice-head td {
+		padding: 0 8px;
+		}
+		.container {
+		padding-top:30px;
+		}
+		.invoice-body{
+		background-color:transparent;
+		}
+		.invoice-thank{
+		margin-top: 60px;
+		padding: 5px;
+		}
+		address{
+		margin-top:15px;
+		}
+		.already-paid * {text-decoration: line-through}
+		#alreadyPaid {position:absolute;top:30%;left:30%;color:red;border:8px solid red;font-size:185px;margin-top: -50px;text-decoration: none;}
+	</style>
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link rel="shortcut icon" href="<?= $icon ?>">
+	</head>
 
-    <link rel="stylesheet" href="/global/css/bootstrap.css">
-    <style>
-      .invoice-head td {
-        padding: 0 8px;
-      }
-      .container {
-      	padding-top:30px;
-      }
-      .invoice-body{
-      	background-color:transparent;
-      }
-      .invoice-thank{
-      	margin-top: 60px;
-      	padding: 5px;
-      }
-      address{
-      	margin-top:15px;
-      }
-    </style>
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <link rel="shortcut icon" href="/favicon.ico">
-	   <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-  </head>
-
-  <body>
-    <div class="container">
-    	<div class="row">
-    		<div class="span4">
-    			<img src="/img/5marks-logo.png" title="logo">
-    			<address>
-			        <strong>5marks, LLC.</strong><br>
-			        P.O Box 3171<br>
-			        Kent, WA 98089<br>
-		    	</address>
-    		</div>
-    		<div class="span4 well">
-    			<table class="invoice-head">
-    				<tbody>
-    					<tr>
-    						<td class="pull-right"><strong>Customer #</strong></td>
-    						<td><?php echo $invoice->customer; ?></td>
-    					</tr>
-    					<tr>
-    						<td class="pull-right"><strong>Invoice #</strong></td>
-    						<td><?php echo $invoice->id; ?></td>
-    					</tr>
-    					<tr>
-    						<td class="pull-right"><strong>Date</strong></td>
-    						<td><?php echo date('M j, Y', $invoice->date); ?></td>
-    					</tr>
-    					<tr>
-    						<td class="pull-right"><strong>Period</strong></td>
-    						<td><?php echo date('M j, Y', $invoice->period_start) .' to ' . date('M j, Y', $invoice->period_end); ?></td>
-    					</tr>
-    				</tbody>
-    			</table>
-    		</div>
-    	</div>
-    	<div class="row">
-    		<div class="span8">
-    			<h2>Invoice</h2>
-    		</div>
-    	</div>
-    	<div class="row">
-		  	<div class="span8 well invoice-body">
-		  		<table class="table table-bordered">
+	<body>
+	<div class="container">
+<?php
+	if($cardError) {
+			echo "<div id='error'>That card has been declined, double check that you entered it correctly!</div>";
+	}
+	if($showThanks) {
+		echo "<h1 id='thanks'>Thanks!</h1>";
+	}
+?>
+		<div class="row">
+			<div class="col col-lg-4 col-lg-offset-2">
+				<a href="<?= $baseURL; ?>">
+					<img src="<?= $baseURL;?>/assets/img/favicon.png" title="logo"><br/>
+					<strong><?= $companyName ?></strong>
+				</a>
+				<br/>
+				<address>
+					<?= $address ?>
+				</address>
+			</div>
+			<div class="col col-lg-4 well">
+				<table class="invoice-head">
+					<tbody>
+						<tr>
+							<td class="pull-right"><strong>Customer</strong></td>
+							<td><?php echo $customer->email; ?></td>
+						</tr>
+						<tr>
+							<td class="pull-right"><strong>Invoice #</strong></td>
+							<td><?php echo $invoice->id; ?></td>
+						</tr>
+						<tr>
+							<td class="pull-right"><strong>Date</strong></td>
+							<td><?php echo date('M j, Y', $invoice->date); ?></td>
+						</tr>
+						<tr>
+							<td class="pull-right"><strong>Period</strong></td>
+							<td><?php echo date('M j, Y', $invoice->period_start) .' to ' . date('M j, Y', $invoice->period_end); ?></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col col-lg-8 col-lg-offset-2">
+				<h2>Invoice</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col col-lg-8 col-lg-offset-2 well invoice-body<?= $alreadyPaid ? " already-paid" : "" ?>">
+				<table class="table table-bordered table-striped">
 					<thead>
 						<tr>
 							<th>Description</th>
@@ -98,13 +139,21 @@
 						</tr>
 					</thead>
 					<tbody>
-
 				<?php
 					$total = 0;
+					foreach($invoice->lines->data as $line){
+						echo '<tr>';
+						$amount = $line->amount / 100;
+						echo '<td>'.$line->description.'</td>';
+						echo '<td>' . date('M j, Y', $line->period->start) .' - ' . date('M j, Y', $line->period->end). '</td>';
+						echo '<td>$' . number_format($amount,2).'</td>';
+						$total += $amount;
+						echo '</tr>';
+					}
 					foreach($invoice->lines->subscriptions as $subscription){
 						echo '<tr>';
 						$amount = $subscription->amount / 100;
-						echo '<td>Agora '.$subscription->plan->name.' ($'.number_format($subscription->plan->amount / 100,2).'/'.$subscription->plan->interval.')</td>';
+						echo '<td>'.$subscription->plan->name.' ($'.number_format($subscription->plan->amount / 100,2).'/'.$subscription->plan->interval.')</td>';
 						echo '<td>' . date('M j, Y', $subscription->period->start) .' - ' . date('M j, Y', $subscription->period->end). '</td>';
 						echo '<td>$' . number_format($amount,2).'</td>';
 						$total += $amount;
@@ -127,28 +176,48 @@
 						</tr>
 					</tbody>
 				</table>
-		  	</div>
-  		</div>
-  		<div class="row">
-  			<div class="span6 offset1 well invoice-thank">
-  				<h5 style="text-align:center;">Thank You!</h5>
-  			</div>
-  		</div>
-  		<div class="row">
-  	    	<div class="span3">
-  		        <strong>Phone:</strong> <a href="tel:555-555-5555">555-555-5555</a>
-  	    	</div>
-  	    	<div class="span3">
-  		        <strong>Email:</strong> <a href="mailto:hello@5marks.co">hello@5marks.co</a>
-  	    	</div>
-  	    	<div class="span3">
-  		        <strong>Website:</strong> <a href="http://5marks.co">http://5marks.co</a>
-  	    	</div>
-  		</div>
-    </div>
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	  <script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery/jquery-1.7.1.min.js"%3E%3C/script%3E'))</script>	
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-  </body>
+				<?= $alreadyPaid ? "<h1 id='alreadyPaid'>PAID</h1>" : "" ?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col col-lg-8 col-lg-offset-2 offset1 well invoice-thank centered">
+<?php if(!$alreadyPaid) { ?>
+				<form method="POST" style="text-align:center;">
+					<input name="i" id="i" value="<?= $invoiceID ?>" type="hidden"/>
+					<script
+					src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+					data-key="<?= $publicKey ?>"
+					data-email="<?= $customer->email ?>"
+					data-image="<?= $icon ?>"
+					data-name="<?= $companyName ?>"
+					data-description="Invoice <?= $invoiceID ?>"
+					data-amount="<?= $invoice->total ?>">
+					</script> securely through <a href="//stripe.com"><img src="https://stripe.com/img/navigation/logo.png?1"></a>
+				</form>
+<?php } ?>
+				<h5 style="text-align:center;">Thank You!</h5>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col col-lg-8 col-lg-offset-2">
+				<div class="col col-lg-4">
+					<strong>Phone:</strong><br/><a href="tel:<?= $phone ?>"><?= $phone ?></a>
+				</div>
+				<div class="col col-lg-4">
+					<strong>Email:</strong><br/><a href="mailto:<?= $baseEmail ?>"><?= $baseEmail ?></a>
+				</div>
+				<div class="col col-lg-4">
+					<strong>Website:</strong><br/><a href="<?= $baseURL ?>"><?= $baseURL ?></a>
+				</div>
+			</div>
+		</div>
+	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	</body>
 </html>
+<?php
+//end of outputInvoice
+} else {
+	header( 'Location: '.$baseURL );
+}
+?>
